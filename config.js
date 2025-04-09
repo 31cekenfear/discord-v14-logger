392c83e5e9e80ec3a7923937d7606eb19af9a67f
31cekenfear <fearteam54@gmail.com>
2025-04-09 13:42:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-token: "Discord Bot Tokeni", // Discord Bot Tokeni
-presence: "Botun Durumu", // Bot'un Durumu
-logChannelID: "Log Kanalının ID'si", // Log Kanal ID'si
+token: "MTM1OTQ3NjczODEyNzgyNzAyNA.G06Ddz.HiJVSN-ohUPsebCWeoIVoPM4TF0LGyvOr7mp70", // Discord Bot Tokeni
+presence: "/vladimir", // Bot'un Durumu
+logChannelID: "1345336714348204035", // Log Kanal ID'si
 statSystem: true, // Offline,Online Status Logu - (⚠️ Yüksek Üyeli Sunucularda ' false ' Olarak Ayarlayın)
 }
